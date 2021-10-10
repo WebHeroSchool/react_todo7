8a900e2ad39975d71367804564402cb509a5cd50
Olesia <ivanova.ov_87@mail.ru>
2021-10-10 17:20:30 +0300

    new file:   src/components/App/App.js 	new file:   src/components/Footer/Footer.js 	new file:   src/components/InputItem/InputItem.js 	new file:   src/components/Item/Item.js 	new file:   src/components/ItemList/ItemList.js

---
+++ todo/src/components/Footer/Footer.js
@@ -0,0 +1,3 @@
+import React from 'react';
+const Footer = () => (<p>Еще одно важное дело</p>);
+export default Footer;
---
+++ todo/src/components/ItemList/ItemList.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Item from '../Item/Item';
+const ItemList = () => (<ul>
+  <li><Item /></li>
+  <li><Item /></li>
+  <li><Item /></li>
+   </ul>);
+export default ItemList;
