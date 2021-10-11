4b4b0627c192a2a05d19a8c8c9166ef3bf3ebcc7
Olesia <ivanova.ov_87@mail.ru>
2021-10-11 14:23:11 +0300

    modified:   src/components/App/App.js 	modified:   src/components/Item/Item.js 	modified:   src/components/ItemList/ItemList.js

--- todo/src/components/App/App.js
+++ todo/src/components/App/App.js
@@ -5,12 +5,27 @@ import Footer from '../Footer/Footer';
 
 const todoItem = 'Написать новое приложение';
 
-  const App = () => (
-    <div>
-    <h1>Важные дела:</h1>
-    <InputItem />
-    <ItemList todoItem={todoItem} />
-    <Footer count={6} />
-    </div>);
+  const App = () => {
+    const items = [
+      {
+        value: 'Написать новое приложение'
+      },
+      {
+        value: 'Прописать props'
+      },
+      {
+        value: 'Сделать все дела'
+      }
+    ];
+
+    return (
+      <div>
+        <h1>Важные дела:</h1>
+        <InputItem />
+        <ItemList items={items} />
+        <Footer count={6} />
+      </div>);
+    }
+
 
   export default App;
