1cce4e49dabe3bce23271763fb40d6359561de2b
Olesia <ivanova.ov_87@mail.ru>
2021-10-11 15:13:35 +0300

    modified:   src/components/App/App.js

--- todo/src/components/App/App.js
+++ todo/src/components/App/App.js
@@ -3,8 +3,6 @@ import ItemList from '../ItemList/ItemList';
 import InputItem from '../InputItem/InputItem';
 import Footer from '../Footer/Footer';
 
-const todoItem = 'Написать новое приложение';
-
   const App = () => {
     const items = [
       {
