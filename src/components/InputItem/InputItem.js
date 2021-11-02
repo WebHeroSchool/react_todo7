806b82697cea0125730ed94b6d5a72b6d9e9fdd4
Lesenok2807 <73076953+Lesenok2807@users.noreply.github.com>
2021-11-02 20:08:05 +0300

    Update InputItem.js

--- src/components/InputItem/InputItem.js
+++ src/components/InputItem/InputItem.js
@@ -1,12 +1,44 @@
 import React from 'react';
 import TextField from '@material-ui/core/TextField';
+import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
 
-const InputItem = () => (<div>
-     <TextField
-       id="standard-basic"
-       label="ДОБАВИТЬ ЗАДАНИЕ"
-       variant="standard"
-     />
-</div>)
+
+
+
+class InputItem extends React.Component {
+  state = {
+    inputValue: ''
+  };
+
+  onButtonClick = () => {
+    this.setState({
+      inputValue: ''
+    });
+
+    this.props.onClickAdd(this.state.inputValue);
+  }
+
+  render() {    
+    return (<Grid>
+         <TextField
+           id="standard-basic"
+           label="ДОБАВИТЬ ЗАДАНИЕ"
+           variant="standard"
+           fullWidth
+           value={this.state.inputValue}
+           onChange={event => this.setState({ inputValue: event.target.value })}
+         />
+         <Button
+           variant='contained'
+           color='primary'
+           fullWidth
+           onClick={this.onButtonClick}
+         >
+         ДОБАВИТЬ
+         </Button>
+    </Grid>);
+  }
+}
 
 export default InputItem;
