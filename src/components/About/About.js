9c908ef6046356e112ca098a7c2b046f775c1b2b
Olesia <ivanova.ov_87@mail.ru>
2021-11-11 22:02:06 +0300

    new file:   src/components/About/About.js 	new file:   src/components/About/About.module.css 	new file:   src/components/Contacts/Contacts.js 	new file:   src/components/Contacts/Contacts.module.css 	new file:   src/components/Todo/Todo.js 	new file:   src/components/Todo/Todo.module.css

---
+++ src/components/About/About.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from './About.module.css';
+
+const About = () => (
+      <div className={styles.main}>
+
+          <h1 className={styles.title}>ABOUT</h1>
+          
+      </div>);
+
+export default About;
---
+++ src/components/Contacts/Contacts.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './Contacts.module.css';
+
+const Contacts = () => (
+      <div className={styles.main}>
+
+          <h1 className={styles.title}>CONTACTS</h1>
+          
+      </div>);
+export default Contacts;
